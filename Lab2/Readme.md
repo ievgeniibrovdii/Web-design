45af6250c40af0a11192c08b34db0e972dc9bab9
ievgeniibrovdii <ievgeniibrovdii@gmail.com>
2018-12-03 01:00:12 +0200

    Lab2 is finished

---
+++ Lab2/Readme.md
@@ -0,0 +1,10 @@
+Лабораторна робота №2
+   з Web-дизайну
+Тема: Освоєння мови Javascript
+  Варіант 4,
+Студента групи КВ-61,
+Бровдія Євгенія
+
+![Image alt](https://github.com/ievgeniibrovdii/Web-design/blob/master/Lab2/screenshots/screenshot1.png)
+![Image alt](https://github.com/ievgeniibrovdii/Web-design/blob/master/Lab2/screenshots/screenshot2.png)
+
---
+++ Lab2/main.js
@@ -0,0 +1,114 @@
+
+var editorModule = (function() {
+    
+    var editor;
+    var textUpperCase = false;
+    
+    function init(){
+        var elem = document.getElementById("fontSizeChanger");
+        var s = "";        
+        for(var i=1; i<15; i++){
+                    s = s + "<option value='"+i+"'>"+i+"</option>";
+        }
+        elem.innerHTML = s;
+    }
+    
+    function editingMethods() {
+        window.addEventListener("load", function(){
+            editor = textingEditor.document;
+            editor.designMode = "on";
+
+            redaction();
+            specialButtons();
+            changeColor();
+            linking();
+            sizeChanger();
+            showResult();
+        }, false);
+    };
+    
+    function redaction(){
+        boldButton.addEventListener("click", function(){
+                editor.execCommand("Bold", false, null);
+            }, false);
+
+            italicButton.addEventListener("click", function(){
+                editor.execCommand("Italic", false, null);
+            }, false);
+
+            underlineButton.addEventListener("click", function(){
+                editor.execCommand("Underline", false, null);
+            }, false);
+
+            strikeButton.addEventListener("click", function(){
+                editor.execCommand("Strikethrough", false, null);
+            }, false);
+    }
+    
+    function specialButtons(){
+        orderedListButton.addEventListener("click", function(){
+                editor.execCommand("InsertOrderedList", false, "new" + Math.round(Math.random()));
+            }, false);
+
+            registerButton.addEventListener("click", function(){
+
+                if(textUpperCase == false) {
+                    editor.body.innerHTML = editor.body.innerHTML.toUpperCase();
+                    textUpperCase = true;
+                } else {
+                    editor.body.innerHTML = editor.body.innerHTML.toLowerCase();
+                    textUpperCase = false;
+                }
+            }, false);
+    }
+    
+    
+    function changeColor(){
+        fontColor.addEventListener("change", function(event){
+                editor.execCommand("Forecolor", false, event.target.value);
+            }, false);
+
+            highlightButton.addEventListener("change", function(event){
+                editor.execCommand("BackColor", false, event.target.value);
+            }, false);
+    }
+    
+    function linking(){
+            linkButton.addEventListener("click", function(){
+                var url = prompt("Введіть адресу:", "http://");
+                editor.execCommand("CreateLink", false, url);
+            }, false);
+
+            unlinkButton.addEventListener("click", function(){
+                editor.execCommand("UnLink", false, null);
+            }, false);
+    }
+    
+    function sizeChanger() {
+        fontSizeChanger.addEventListener("change", function(event){
+                editor.execCommand("FontSize", false, event.target.value);
+            }, false);
+    }
+    
+    function showResult(){
+        showButton.addEventListener("click", function(){
+                my_window = window.open("Editor", "windowLab2");
+                my_window.document.write(editor.body.innerHTML);
+            }, false);
+    }
+    
+    init();
+    
+    return {
+        startEditing: function() {
+            editingMethods();
+        }
+    }; 
+    
+}());
+
+editorModule.startEditing();
+
+
+
+
